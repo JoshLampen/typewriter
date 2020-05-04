09b0b6695e8380cbb3a11c863ba6abc9c32543ef
JoshLampen <joshlampen2@gmail.com>
2020-05-04 22:52:48 +0000

    Small change

--- typewriter.js
+++ typewriter.js
@@ -3,5 +3,5 @@ const sentence = "hello there from lighthouse labs";
 for (let i = 0; i < sentence.length; i++) {
   setTimeout(() => {
     i === sentence.length - 1 ? process.stdout.write(sentence[i] + '\n') : process.stdout.write(sentence[i]);
-  }, 50 + i * 50);
+  }, i * 50);
 }
